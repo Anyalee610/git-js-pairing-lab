b457c6fd24f7c11893ed4379c371271c872dfd27
Anyalee610 <anayalambright610@gmail.com>
2022-03-23 17:54:04 +0000

    frame unit Q 8

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 //Code your solutions in this file
+
+//1
+console.log("Q1-----")
+function fiveToOneHundred(){
+    
+}
+fiveToOneHundred()
+
+
+//2 
+console.log('Q2')
+function multiplesOfThree(){
+    
+}
+
+multiplesOfThree()
+
+//3
+console.log('Q3')
+function multiplesOfThreeOrFive(){
+    
+}
+
+multiplesOfThreeOrFive()
+
+//4
+console.log('Q4')
+function untilNum(){
+    
+}
+
+untilNum()
+
+//5 
+console.log('Q5-----')
+function multiply(){
+    
+}
+multiply()
+
+//6 
+console.log('Q6')
+function add(){
+    
+}
+add()
+
+//7 
+console.log('Q7----')
+function isNegative(){
+    
+}
+isNegative()
+
+//8 
+console.log('Q8---')
+function triangleArea(){
+    
+}
+triangleArea()
