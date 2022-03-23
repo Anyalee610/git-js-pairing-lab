5e258bf789eeb8f962a99df02cc41dc373841000
Anyalee610 <anayalambright610@gmail.com>
2022-03-23 19:23:01 +0000

    q5

--- index.js
+++ index.js
@@ -52,30 +52,46 @@ function untilNum(num)
 
 untilNum(10)
 
-//5 
+//5 `multiply` takes in two number parameters and **returns** their product.
 console.log('Q5-----')
-function multiply(){
+function multiply(a, b){
+    return a*b
     
 }
-multiply()
+console.log(multiply(2, 2))
 
-//6 
+//6 `add` that takes in two number parameters and **returns** their sum. But if the two values are the same, return **triple their sum**!
 console.log('Q6')
 function add(){
     
 }
 add()
 
-//7 
+//7  `isNegative` that takes in a number parameter and **returns** `true` is the number is a negative value and `false` is the number is a positive value. 
 console.log('Q7----')
 function isNegative(){
     
 }
 isNegative()
 
-//8 
+//8 triangleArea` that takes in the height and base of a triangle and **returns** the [area of the triangle](https://www.mathsisfun.com/algebra/trig-area-triangle-without-right-angle.html).
 console.log('Q8---')
 function triangleArea(){
     
 }
+
 triangleArea()
+
+//9 takes in a number parameter and checks whether a given integer is within 20 of 100. It **returns** `true` if it is and `false` if not.
+console.log('Q9----')
+function betweenTwentyAndFourty(){
+    
+}
+betweenTwentyAndFourty()
+
+//10 `largest` that takes in three number parameters and **returns** the largest of the values.
+console.log('10----')
+function Largest(){
+    
+}
+Largest()
