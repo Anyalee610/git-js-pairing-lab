6525e9c676e631ccd1ae5e785e0d246381a02af2
Anyalee610 <anayalambright610@gmail.com>
2022-03-23 19:42:02 +0000

    Q16 frame

--- index.js
+++ index.js
@@ -61,16 +61,16 @@ function multiply(a, b){
 console.log(multiply(2, 2))
 
 //6 `add` that takes in two number parameters and **returns** their sum. But if the two values are the same, return **triple their sum**!
-console.log('Q6')
+console.log('Q6');
 function add(a,b){
     if (a === b){
-        return (a+b)*3
+        return (a+b)*3;
     }else {
-        return a+b 
-        
+        return a+b;
+    }
     
 }
-console.log(add(2,2))
+console.log(add(2,2));
 
 //7  `isNegative` that takes in a number parameter and **returns** `true` is the number is a negative value and `false` is the number is a positive value. 
 console.log('Q7----')
@@ -79,10 +79,26 @@ function isNegative(){
 }
 isNegative()
 
-//8 triangleArea` that takes in the height and base of a triangle and **returns** the [area of the triangle](https://www.mathsisfun.com/algebra/trig-area-triangle-without-right-angle.html).
+//8 
 console.log('Q8---')
 function triangleArea(){
     
 }
-
 triangleArea()
+//9 takes in a number parameter and checks whether a given integer is within 20 of 100. It **returns** `true` if it is and `false` if not.
+console.log('Q9----')
+function betweenTwentyAndFourty(){
+    
+}
+betweenTwentyAndFourty()
+
+//10 `largest` that takes in three number parameters and **returns** the largest of the values.
+console.log('10----')
+function Largest(){
+    
+}
+Largest()
+
+//Write a function `changeString` to replace every character in a given string with the character following it in the alphabet/
+console.log('Q16')
+function changeString
